06a038dec2169cc87d74f45ab0a97bdc5218b656
michael <evtushenkomisha21@gmail.com>
2019-11-02 20:25:47 +0200

    Revert "Fixed Authentication filter"
    This reverts commit 5f857711b9325ae75878078a99cb2b86d56c35b0.

--- src/main/java/com/netcraker/security/SecurityConstants.java
+++ src/main/java/com/netcraker/security/SecurityConstants.java
@@ -4,6 +4,6 @@ public class SecurityConstants {
     public static final String TOKEN_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String AUTH_LOGIN_URL = "auth/login";
-    public static final String SECRET_KEY = "n2r5u8x/A%D*G-KaPdSgVkYp3s6v9y$B&E(H+MbQeThWmZq4t7w!z%C*F-J@NcRf";//"256BIT_EXAMPLE_SECRET_KEY"
+    public static final String SECRET_KEY = "256BIT_EXAMPLE_SECRET_KEY";
     public static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 14; // 14 days
 }
--- src/main/java/com/netcraker/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/netcraker/security/filter/JwtAuthorizationFilter.java
@@ -3,8 +3,6 @@ package com.netcraker.security.filter;
 
 import com.netcraker.security.SecurityConstants;
 import io.jsonwebtoken.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,19 +11,15 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
-
-import java.util.stream.Collectors;
 
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
-    private static  final Logger log = LoggerFactory.getLogger(JwtAuthorizationFilter.class);
+
     public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
         System.out.println("JwtAuthorizationFilter constructed ");
@@ -48,52 +42,25 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
     }
 
     private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
-        String token = request.getHeader(SecurityConstants.TOKEN_HEADER);
+        String header = request.getHeader(SecurityConstants.TOKEN_HEADER);
 
-        if (!StringUtils.isEmpty(token) && token.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+        if (header != null && header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
             try {
-                byte[] signingKey = SecurityConstants.SECRET_KEY.getBytes();
-                Jws<Claims> parsedToken = Jwts.parser()
+                String token = header.substring(SecurityConstants.TOKEN_PREFIX.length());
+
+                String signingKey = SecurityConstants.SECRET_KEY;
+
+                Claims parsedClaims = Jwts.parser()
                         .setSigningKey(signingKey)
-                        .parseClaimsJws((token.replace("Bearer", "")));
+                        .parseClaimsJws(token)
+                        .getBody();
 
-                String username = parsedToken.getBody().getSubject();
+                String username = parsedClaims.getSubject();
 
-                List authorities = ((List<?>) parsedToken.getBody()
-                        .get("rol")).stream()
-                        .map(authority -> new SimpleGrantedAuthority((String) authority))
-                        .collect(Collectors.toList());
-                if (!StringUtils.isEmpty(username)) {
-                    return new UsernamePasswordAuthenticationToken(username, null, authorities);
+                if (username != null && username.length() > 0) {
+                    return new UsernamePasswordAuthenticationToken(username, null, null);
                 }
-            }catch (ExpiredJwtException exception) {
-                log.warn("Request to parse expired JWT : {} failed : {}", token, exception.getMessage());
-            }catch(UnsupportedJwtException exception) {
-                log.warn("Request to parse unsupported JWT : {} failed : {}", token, exception.getMessage());
-            }catch (MalformedJwtException exception) {
-                log.warn("Request to parse invalid JWT : {} failed : {}", token, exception.getMessage());
-            }catch(SignatureException exception) {
-                log.warn("Request to parse JWT with invalid signature : {} failed : {}", token, exception.getMessage());
-            }catch (IllegalArgumentException exception){
-                log.warn("Request to parse empty or null JWT : {} failed : {}", token, exception.getMessage());
             }
-
-
-//                String token = header.substring(SecurityConstants.TOKEN_PREFIX.length());
-//
-//                String signingKey = SecurityConstants.SECRET_KEY;
-//
-//                Claims parsedClaims = Jwts.parser()
-//                        .setSigningKey(signingKey)
-//                        .parseClaimsJws(token)
-//                        .getBody();
-//
-//                String username = parsedClaims.getSubject();
-
-//                if (username != null && username.length() > 0) {
-//                    return new UsernamePasswordAuthenticationToken(username, null, null);
-//                }
-
 //            catch (ExpiredJwtException exception) {
 ////                s("Request to parse expired JWT : {} failed : {}", token, exception.getMessage());
 //            } catch (UnsupportedJwtException exception) {
@@ -105,9 +72,9 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 //            } catch (IllegalArgumentException exception) {
 ////                log.warn("Request to parse empty or null JWT : {} failed : {}", token, exception.getMessage());
 //            }
-//            catch (Exception e){
-//                e.printStackTrace();
-//            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
         }
         return null;
     }

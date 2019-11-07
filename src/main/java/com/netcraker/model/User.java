857595b76adf089d5eabcb60f3e9e93e5745acab
BondarOleg98 <olegb5195@gmail.com>
2019-11-07 13:31:12 +0200

    fixed code style

--- src/main/java/com/netcraker/model/User.java
+++ src/main/java/com/netcraker/model/User.java
@@ -3,9 +3,9 @@ package com.netcraker.model;
 import lombok.*;
 import java.sql.Timestamp;
 
-@Getter
-@Setter
-public class User {
+
+@Getter @Setter
+public class User{
     private int userId;
     private String full_name;
     private String email;

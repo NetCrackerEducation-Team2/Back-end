918c990abc11039dcdc209edefd209cbe22b15a8
BondarOleg98 <olegb5195@gmail.com>
2019-11-07 16:10:05 +0200

    Update User.java

--- src/main/java/com/netcraker/model/User.java
+++ src/main/java/com/netcraker/model/User.java
@@ -1,12 +1,16 @@
 package com.netcraker.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private int userId;
     private String full_name;

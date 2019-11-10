e44d7318e8606c8ed6b8a8f41a546f3d68dd9e49
michael <evtushenkomisha21@gmail.com>
2019-11-10 02:12:30 +0200

    Fixed bugs in activation

--- src/main/java/com/netcraker/repositories/UserRepository.java
+++ src/main/java/com/netcraker/repositories/UserRepository.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
@@ -31,43 +34,20 @@ public class UserRepository {
     @Value("${spring.queries.find.user.email}")
     private String sqlSelectUserEmail;
 
-//    private List<User> TEST_USER_LIST = new ArrayList<User>() {
-//        {
-//            final User user = new User();
-//            user.setUserId(0);
-//            user.setFullName("Alpha Beta");
-//            user.setEmail("test@test.com");
-//            user.setPassword("test");
-//            user.setEnabled(true);
-//
-//            add(user);
-//        }
-//    };
 
     public User createUser(User user) {
-        jdbcTemplate.update(sqlCreateUser, new Object[] { user.getFullName(), user.getPassword(), user.getEmail(),
-                                        new Timestamp(System.currentTimeMillis()), true, user.getPhotoPath()});
-//        user.setUserId(TEST_USER_LIST.size());
-//        TEST_USER_LIST.add(user);
+        jdbcTemplate.update(sqlCreateUser, new Object[]{user.getFullName(), user.getPassword(), user.getEmail(),
+                new Timestamp(System.currentTimeMillis()), true, user.getPhotoPath()});
+        user = findByEmail(user.getEmail());
+        System.out.println("Got id:" + user.getUserId());
         return user;
     }
 
     public User findByEmail(String email) {
         return jdbcTemplate.queryForObject(sqlSelectUserEmail, new Object[]{email}, new UserRowMapper());
-//        for (User u : TEST_USER_LIST) {
-//            if (u.getEmail().equals(email)) {
-//                return u;
-//            }
-//        }
     }
 
     public User findByUserId(int userId) {
-//        for(User u: TEST_USER_LIST){
-//            if(u.getUserId()==userId){
-//                return u;
-//            }
-//        }
-//        return null;
         return jdbcTemplate.queryForObject(sqlSelectUserId, new Object[]{userId}, new UserRowMapper());
     }
 }
--- src/main/java/com/netcraker/services/impl/UserServiceImpl.java
+++ src/main/java/com/netcraker/services/impl/UserServiceImpl.java
@@ -12,6 +12,7 @@ import com.netcraker.services.UserService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -27,7 +28,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User createUser(User user) {
-        User userFromDB = userRepository.findByEmail(user.getEmail());
+        User userFromDB = null;
+        try {
+            userFromDB = userRepository.findByEmail(user.getEmail());
+        } catch (DataAccessException ignored) {
+            // it's alright
+        }
 
         if (userFromDB != null) {
             throw new FailedToRegisterException("Email is already used");
@@ -65,12 +71,18 @@ public class UserServiceImpl implements UserService {
     }
 
     public boolean activateUser(String token) {
-        AuthorizationLinks authorizationLinks = authorizationRepository.findByActivationCode(token);
+        AuthorizationLinks authorizationLinks;
+        try {
+            authorizationLinks = authorizationRepository.findByActivationCode(token);
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            return false;
+        }
 
         if (authorizationLinks == null) {
             return false;
         }
-
+        System.out.println("Auth link has user's id:" + authorizationLinks.getUserId());
         User user = userRepository.findByUserId(authorizationLinks.getUserId());
 
         if (user == null) {

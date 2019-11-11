bd79c4922a720d93c8bc08761611766e3cfcd173
yura <uraura05052000@gmail.com>
2019-11-11 11:03:50 +0200

    Some lombok annotations added

--- src/main/java/com/netcraker/model/Genre.java
+++ src/main/java/com/netcraker/model/Genre.java
@@ -2,11 +2,12 @@ package com.netcraker.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Getter @Setter @Builder
 public class Genre {
     private int genreId;
-    private String name;
+    private @NonNull String name;
     private String description;
 }

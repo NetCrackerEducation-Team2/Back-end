5fb915076c6d8c55ab8f5d3ebb29d567528ec129
BondarOleg98 <olegb5195@gmail.com>
2019-11-15 14:00:02 +0200

    Change activation link

--- src/main/java/com/netcraker/services/MailSender.java
+++ src/main/java/com/netcraker/services/MailSender.java
@@ -33,7 +33,7 @@ public class MailSender {
         return String.format(
                 "Hello, %s! \n" +
                         "Welcome to library. " +
-                        "Please visit next link: https://netcracker2-back-end.herokuapp.com%s/%s",
+                        "Please visit next link: https://netcracker2-back-end.herokuapp.com/auth%s/%s",
                 fullName, SecurityConstants.AUTH_ACTIVATION_URL, token
         );
     }

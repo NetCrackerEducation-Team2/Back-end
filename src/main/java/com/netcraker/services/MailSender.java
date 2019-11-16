799147fdef8cd8eb5ecdec3739e7c059caffba64
BondarOleg98 <olegb5195@gmail.com>
2019-11-16 22:43:54 +0200

    Update url in mailSender

--- src/main/java/com/netcraker/services/MailSender.java
+++ src/main/java/com/netcraker/services/MailSender.java
@@ -35,7 +35,7 @@ public class MailSender {
         return String.format(
                 "Hello, %s! \n" +
                         "Welcome to library. " +
-                        "Please visit next link: https://netcracker2-front-end.herokuapp.com:4200%s/%s",
+                        "Please visit next link: https://netcracker2-front-end.herokuapp.com%s/%s",
                 fullName, SecurityConstants.AUTH_ACTIVATION_URL, token
         );
     }

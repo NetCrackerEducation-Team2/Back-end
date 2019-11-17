bdc14da5ba82303e9d44d62f5b9cece80afd0625
Volodymyr Balamut <volodymyrbalamut@gmail.com>
2019-11-17 16:53:39 +0200

    Fixed some bugs

--- src/main/java/com/netcraker/model/BookOverview.java
+++ src/main/java/com/netcraker/model/BookOverview.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookOverview {
     private int bookOverviewId;
     private @NonNull String description;

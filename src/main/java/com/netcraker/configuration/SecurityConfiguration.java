e16f89fbec091a632e4b6a2525fcb56b42aa57e4
BondarOleg98 <olegb5195@gmail.com>
2019-12-17 02:41:38 +0200

    Minimal refactoring

--- src/main/java/com/netcraker/configuration/SecurityConfiguration.java
+++ src/main/java/com/netcraker/configuration/SecurityConfiguration.java
@@ -56,8 +56,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     protected PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-//        return NoOpPasswordEncoder.getInstance();
+//        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Override
